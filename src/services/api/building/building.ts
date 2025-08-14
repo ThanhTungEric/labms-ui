d9a992814548eeb6c76131f264f3952af64981c8
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2025-08-14 09:26:17 +0700

    [UPDATE] building

--- src/services/api/building/building.ts
+++ src/services/api/building/building.ts
@@ -6,3 +6,8 @@ export async function getAllBuildings(): Promise<Building[]> {
   const response = await api.get<Building[]>('/buildings');
   return response.data;
 }
+
+export async function getBuildingById(id: number): Promise<Building> {
+  const response = await api.get<Building>(`/buildings/${id}`);
+  return response.data;
+}
